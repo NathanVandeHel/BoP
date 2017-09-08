c36beab5c394489205a4b065418656e3541ad525
NathanVandeHel <nathan.vandehel@gmail.com>
2017-09-08 18:10:01 +0200

    firt commit

---
+++ tp1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14159265
+
+int main()
+{
+    double d, h, a, n;
+    printf("Paint the container \n");
+    printf("Height ? ");
+    scanf("%lf", &h);
+    printf("Diameter ? ");
+    scanf("%lf", &d);
+    a = 2*PI*pow(d/2,2) + PI*d*h;
+    n = a/2;
+    printf("%.1f can(s) of paint needed", n);
+
+    return 0;
+}
